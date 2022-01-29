97fec35a936e90a9e873493b9b4add48809f8335
Hamza Ali Khan <85096025+ErenYea@users.noreply.github.com>
2022-01-29 14:12:50 +0500

    server

---
+++ routes/data.js
@@ -0,0 +1,54 @@
+const data = [
+    {
+        id:1,
+        image:{url:"https://i5.walmartimages.com/asr/2896d138-e109-45cb-b59c-da039798533a.68d2949e99b45b72e8d9a684eb99b570.png?odnHeight=612&odnWidth=612&odnBg=FFFFFF"},
+        name:"Horizon Forbidden West Launch",
+        price:"$69.99",
+        product:{url:"https://www.walmart.com/ip/Horizon-Forbidden-West-Launch-Edition-Playstation-5/983372308"}
+    },
+    {
+        id:2,
+        image:{url:"https://i5.walmartimages.com/asr/8c786663-1bd2-4a83-8f75-da7cef0793f8.e12e00758596358a8243e1bfc6f73e44.png?odnHeight=612&odnWidth=612&odnBg=FFFFFF"},
+        name:"Elden Ring, Bandai Namco, PlayStation 5",
+        price:"$59.99",
+        product:{url:"https://www.walmart.com/ip/Elden-Ring-Bandai-Namco-PlayStation-5-Physical-722674130202/979908708"}
+    },
+    {
+        id:3,
+        image:{url:"https://i5.walmartimages.com/asr/2736acff-0239-4f57-9677-b9b194e3bb96.1306073a4dd1a3874d5a8ecf6abebe9f.png?odnHeight=612&odnWidth=612&odnBg=FFFFFF"},
+        name:"DualSense Wireless Controller - Starlight Blue",
+        price:"$74.00",
+        product:{url:"https://www.walmart.com/ip/DualSense-Wireless-Controller-Starlight-Blue/844540146"}
+    },
+    {
+        id:4,
+        image:{url:"https://i5.walmartimages.com/asr/21d776f3-018e-4ba3-878d-73042b187096.14f7e11bf00d0f5f0061fc65ca506ec5.png?odnHeight=612&odnWidth=612&odnBg=FFFFFF"},
+        name:"DualSense Wireless Controller - Nova Pink",
+        price:"$74.00",
+        product:{url:"https://www.walmart.com/ip/DualSense-Wireless-Controller-Nova-Pink/331182846"}
+    },
+    {
+        id:5,
+        image:{url:"https://i5.walmartimages.com/asr/cb2a8c08-f802-457f-9718-7af3b17eabc3.bff5908b838de9ef55e881468351aad6.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF"},
+        name:"Turtle Beach Recon 200 GEN 2 PS BLACK",
+        price:"$59.95",
+        product:{url:"https://www.walmart.com/ip/Turtle-Beach-Recon-200-GEN-2-PS-BLACK/486625433?athcpid=486625433&athpgid=AthenaItempage&athcgid=null&athznid=utic&athieid=v0&athstid=CS020&athguid=_vrl_8v5pwQvPoAX4xwwJKFnBgQriNE6V93d&athancid=null&athena=true"}
+    },
+    {
+        id:6,
+        image:{url:"https://i5.walmartimages.com/asr/7b99d3ca-ba8c-4b05-8d40-685cb7943ecd.623a75588901ad79d9cf0afd265f8601.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF"},
+        name:"LYH Pink Gaming Headset ",
+        price:"$9.99",
+        product:{url:"https://www.walmart.com/ip/LYH-Pink-Gaming-Headset-PS4-Xbox-One-Headset-PC-LED-Lights-Detachable-Cat-Ear-Headphones-Compatible-Controller-Adapter-Not-Included-PS4-PS5/555350627?athcpid=555350627&athpgid=AthenaItempage&athcgid=null&athznid=utic&athieid=v0&athstid=CS020&athguid=hhVIxfMywCcY3tAEzilOelMsWAaDKp5smudq&athancid=null&athena=true&athbdg=L1400"}
+    },
+    {
+        id:7,
+        image:{url:"https://i5.walmartimages.com/asr/3b7ed5f3-7e41-4bde-be42-a279e3978e4b.2bb2d1cd66514b9426a609cd6c0dab00.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF"},
+        name:"Uncharted: Legacy of Thieves Collection - PlayStation 5",
+        price:"$49.99",
+        product:{url:"https://www.walmart.com/ip/Uncharted-Legacy-of-Thieves-Collection-PlayStation-5/248103554"}
+    }
+
+];
+
+module.exports= {data};
