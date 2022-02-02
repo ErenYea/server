22e321d1ea69467e52f087c65edd994663f9ebb6
Hamza Ali Khan <drstoneeren@gmail.com>
2022-02-02 14:35:46 +0500

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/data', indexRouter);
-app.use('/user', usersRouter);
+// app.use('/user', usersRouter);
 app.use('/about', aboutuserRouter);
 app.use('/table',tablerouter);
 app.use('/search',searchRouter);
